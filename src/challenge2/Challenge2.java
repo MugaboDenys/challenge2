ad5e3a37edf63975a35074ee4a1d80a29e8b7467
Mugabodenys <mugabodan2020@gmail.com>
2023-05-06 22:05:24 +0200

    made changes to refactor

--- src/challenge2/Challenge2.java
+++ src/challenge2/Challenge2.java
@@ -18,8 +18,8 @@ public class Challenge2 {
         return (650 * 40)/100;
     }
 
-   public static ArrayList<String> calculateEarningsAndPaymentsSeasonB(int[] months) {
-        ArrayList<String> earningsAndPayments = new ArrayList<>();
+   public static ArrayList<String> calcSeasonB(int[] months) {
+        ArrayList<String> earnPay = new ArrayList<>();
         double monthlyPay = 78 * 21;
         double feesToPayToXYZ = monthlyPay * 0.391;
         
@@ -29,25 +29,25 @@ public class Challenge2 {
             double earnings = monthlyPay - discountedFees;
             
             if (discountedFees / (months[i] * 21) > 950) {
-                earningsAndPayments.add("N/A");
+                earnPay.add("N/A");
                 try {
                     throw  new Exception("Cannot pay beyond 950");
                 } catch (Exception ex) {
                     Logger.getLogger(Challenge2.class.getName()).log(Level.SEVERE, null, ex);
                 }
             } else {
-                String result = String.format("%.0f ⇔ %.0f", earnings, discountedFees);
-                earningsAndPayments.add(result);
+                String result = String.format("%.0f <=> %.0f", earnings, discountedFees);
+                earnPay.add(result);
             }
         }
         
-        return earningsAndPayments;
+        return earnPay;
     }
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Season A recruits pay to XYZ: "+calcSeasonA());
            int[] months = {5, 6, 4, 2};
-        ArrayList<String> results = calculateEarningsAndPaymentsSeasonB(months);
+        ArrayList<String> results = calcSeasonB(months);
 
         for (String result : results) {
             System.out.println(result);
